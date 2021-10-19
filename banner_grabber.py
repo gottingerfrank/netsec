bec233baf03a941cf59296fbfb991bf9d4a0eaf0
Frank Gottinger <gottingerfrank@zoho.com>
2021-10-19 17:22:15 +0200

    updated banner_grabber, added tcp_listener scripts

--- banner_grabber.py
+++ banner_grabber.py
@@ -12,8 +12,8 @@ s.connect((host_ip))
 answer = s.recv(1024)
 
 print(f"[+] Received Banner Information of host {host_ip}")
-print(f"**************************** SSH Banner {host_ip} ****************************")
-print(answer)
-print(f"**************************** Banner END **************************************")
+print(f"************************ SSH Banner {host_ip} ************************\n")
+print(answer, "\n")
+print(f"************************ Banner END **********************************\n")
 
 s.close()
---
+++ tcp_listener.py
@@ -0,0 +1,27 @@
+#! /opt/homebrew/bin/python3
+
+import socket
+
+TCP_IP = "192.168.0.1"
+TCP_PORT = 5555
+BUFFER_SIZE = 100 # Adjust Buffer where needed
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+con, addr = s.accept()
+print(f"[*] Connection address: ", addr)
+
+while True:
+    data = con.recv(BUFFER_SIZE)
+
+    if not data:
+        break
+    print(f"[+] Received data: ", data)
+    con.send(data) #echo back received data (test)
+
+con.close()
+print(f"[-] Connection closed ...")
+
