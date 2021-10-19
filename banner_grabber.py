9ec79508cac2a7765cc41f85374673f58544fdba
Frank Gottinger <gottingerfrank@zoho.com>
2021-10-19 16:59:26 +0200

    small cosmetic changes (print()) to ssh banner_grabber

--- banner_grabber.py
+++ banner_grabber.py
@@ -1,17 +1,19 @@
 #! /opt/homebrew/bin/python3
 
-# Simple Banner Grabber
+# Simple SSH Banner Grabber
 
 import socket
 
-host_ip = "192.168.0.1"
+host_ip = ("192.168.0.1", 22) # set host ssh connection tuple (IP_ADDR, PORT)
 
 s = socket.socket()
 s.connect((host_ip))
 
 answer = s.recv(1024)
-print(f"[+] Received Banner Information of host {host_ip}\n")
+
+print(f"[+] Received Banner Information of host {host_ip}")
+print(f"**************************** SSH Banner {host_ip} ****************************")
 print(answer)
+print(f"**************************** Banner END **************************************")
 
 s.close()
-
